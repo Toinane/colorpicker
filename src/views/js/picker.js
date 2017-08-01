cb276ef723f900c57ab762e9cfcf01634adfb122
Toinane <toinane@Toinane-linux.lan>
2017-08-01 23:17:04 +0200

    config for linux

--- src/views/js/picker.js
+++ src/views/js/picker.js
@@ -4,5 +4,6 @@ ipcRenderer.send('picker');
 
 ipcRenderer.on('picker', (event, color) => {
   document.querySelector('body').style.background = color;
+  document.querySelector('body').innerHTML = color;
   ipcRenderer.send('picker');
 });
