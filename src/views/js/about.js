4b4cf575fc76072ee81618aad20cb9534e4f530f
Toinane <toinane@yahoo.fr>
2017-08-09 15:46:16 +0200

    fix(update): get release info from server

--- src/views/js/about.js
+++ src/views/js/about.js
@@ -4,8 +4,17 @@ const {ipcRenderer, shell} = require('electron')
 
 document.addEventListener('DOMContentLoaded', () => ipcRenderer.send('init-about'), false)
 
-ipcRenderer.on('init', (event, config) => {
+ipcRenderer.on('init', (event, version) => {
+  document.querySelector('#version span').innerHTML = version
+})
 
+ipcRenderer.on('update', (event, message) => {
+  document.querySelector('#update').innerHTML = message
+  if (document.querySelector('#update span')) {
+    document.querySelector('#update span').addEventListener('click', function(event) {
+      shell.openExternal(this.getAttribute('data-link'))
+    })
+  }
 })
 
 document.querySelector('h1').addEventListener('click', () => {
