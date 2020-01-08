3a00afd7bbd59eb0dbb48be556a886f120474a7b
toinane <toinane@crea-that.fr>
2020-01-08 13:56:23 +0100

    update dependencies & add instructions to rebuild native module from source

--- src/browsers/colorpicker.js
+++ src/browsers/colorpicker.js
@@ -1,84 +1,100 @@
-'use strict'
+'use strict';
 
-const {BrowserWindow} = require('electron')
+const { BrowserWindow } = require('electron');
 
 module.exports = (dirname, storage, util) => {
-  const {touchbar} = util
-  let win
+    const { touchbar } = util;
+    let win;
 
-  /**
-   * [init]
-   * @param {boolean} force [force launching new window]
-   * @return {void} [new Colorpicker]
-   */
-  let init = (force, color) => {
-    const size = storage.get('size')
-    if (win === null || win === undefined || force) createWindow(size.width, size.height)
-    else win.show()
-  }
+    /**
+     * [init]
+     * @param {boolean} force [force launching new window]
+     * @return {void} [new Colorpicker]
+     */
+    let init = (force, color) => {
+        const size = storage.get('size');
+        if (win === null || win === undefined || force)
+            createWindow(size.width, size.height);
+        else win.show();
+    };
 
-  /**
-   * [createWindow - create new Window]
-   * @param  {int} width  [width of the window]
-   * @param  {int} height [height of the window]
-   * @return {void}
-   */
-  let createWindow = (width, height) => {
-    const pos = storage.get('pos')
-    let options = {
-      frame: false,
-      autoHideMenuBar: true,
-      width: width,
-      height: height,
-      minWidth: 440,
-      minHeight: 150,
-      transparent: true,
-      icon: `${dirname}/build/icon.png`
-    }
-    if (pos) { options.x = pos.x; options.y = pos.y }
+    /**
+     * [createWindow - create new Window]
+     * @param  {int} width  [width of the window]
+     * @param  {int} height [height of the window]
+     * @return {void}
+     */
+    let createWindow = (width, height) => {
+        const pos = storage.get('pos');
+        let options = {
+            frame: false,
+            autoHideMenuBar: true,
+            width: width,
+            height: height,
+            minWidth: 440,
+            minHeight: 150,
+            transparent: true,
+            icon: `${dirname}/build/icon.png`,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        };
+        if (pos) {
+            options.x = pos.x;
+            options.y = pos.y;
+        }
 
-    win = new BrowserWindow(options)
-    win.loadURL(`file://${dirname}/views/colorpicker.html`)
+        win = new BrowserWindow(options);
+        win.loadURL(`file://${dirname}/views/colorpicker.html`);
 
-    if (touchbar) win.setTouchBar(touchbar)
+        if (touchbar) win.setTouchBar(touchbar);
 
-    win.on('closed', () => {
-      win = undefined
-      let totalWin = BrowserWindow.getAllWindows()
-      for (let wins of totalWin) wins.close()
-    })
+        win.on('closed', () => {
+            win = undefined;
+            let totalWin = BrowserWindow.getAllWindows();
+            for (let wins of totalWin) wins.close();
+        });
 
-    windowEvents(win)
-  }
+        windowEvents(win);
+    };
 
-  /**
-   * [windowEvents - BrowserWindow events]
-   * @param  {BrowserWindow} win [current window]
-   * @return {void}
-   */
-  let windowEvents = win => {
-    let timing
+    /**
+     * [windowEvents - BrowserWindow events]
+     * @param  {BrowserWindow} win [current window]
+     * @return {void}
+     */
+    let windowEvents = win => {
+        let timing;
 
-    win.on('focus', event => win.webContents.send('hasLooseFocus', false))
-    win.on('blur', event => win.webContents.send('hasLooseFocus', true))
+        win.on('focus', event => win.webContents.send('hasLooseFocus', false));
+        win.on('blur', event => win.webContents.send('hasLooseFocus', true));
 
-    win.on('resize', event => {
-      const size = win.getBounds()
-      clearTimeout(timing)
-      timing = setTimeout(() => storage.add({size: { width: size.width, height: size.height }}), 300)
-    })
+        win.on('resize', event => {
+            const size = win.getBounds();
+            clearTimeout(timing);
+            timing = setTimeout(
+                () =>
+                    storage.add({
+                        size: { width: size.width, height: size.height }
+                    }),
+                300
+            );
+        });
 
-    win.on('move', event => {
-      const pos = win.getBounds()
-      clearTimeout(timing)
-      timing = setTimeout(() => storage.add({pos: { x: pos.x, y: pos.y }}), 300)
-    })
-  }
+        win.on('move', event => {
+            const pos = win.getBounds();
+            clearTimeout(timing);
+            timing = setTimeout(
+                () => storage.add({ pos: { x: pos.x, y: pos.y } }),
+                300
+            );
+        });
+    };
 
-  let getWindow = () => win
+    let getWindow = () => win;
 
-  return {
-    init: init,
-    getWindow: getWindow
-  }
-}
+    return {
+        init: init,
+        getWindow: getWindow
+    };
+};
--- src/browsers/preview.js
+++ src/browsers/preview.js
@@ -1,47 +1,50 @@
-'use strict'
+'use strict';
 
-const {BrowserWindow} = require('electron')
+const { BrowserWindow } = require('electron');
 
 module.exports = (dirname, storage) => {
-  let win
+    let win;
 
-  /**
-   * [init]
-   * @param {boolean} force [force launching new window]
-   * @return {void} [new Colorpicker]
-   */
-  let init = () => {
-    if (win === null || win === undefined) createWindow()
-    else win.show()
-  }
+    /**
+     * [init]
+     * @param {boolean} force [force launching new window]
+     * @return {void} [new Colorpicker]
+     */
+    let init = () => {
+        if (win === null || win === undefined) createWindow();
+        else win.show();
+    };
 
-  /**
-   * [createWindow - create new Window]
-   * @param  {int} width  [width of the window]
-   * @param  {int} height [height of the window]
-   * @return {void}
-   */
-  let createWindow = () => {
-    let options = {
-      width: 400,
-      height: 300,
-      resizable: false,
-      fullscreenable: false,
-      icon: `${dirname}/build/icon.png`
-    }
+    /**
+     * [createWindow - create new Window]
+     * @param  {int} width  [width of the window]
+     * @param  {int} height [height of the window]
+     * @return {void}
+     */
+    let createWindow = () => {
+        let options = {
+            width: 400,
+            height: 300,
+            resizable: false,
+            fullscreenable: false,
+            icon: `${dirname}/build/icon.png`,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        };
 
-    win = new BrowserWindow(options)
-    win.loadURL(`file://${dirname}/views/preview.html`)
+        win = new BrowserWindow(options);
+        win.loadURL(`file://${dirname}/views/preview.html`);
 
-    win.on('closed', () => {
-      win = undefined
-    })
-  }
+        win.on('closed', () => {
+            win = undefined;
+        });
+    };
 
-  let getWindow = () => win
+    let getWindow = () => win;
 
-  return {
-    init: init,
-    getWindow: getWindow
-  }
-}
+    return {
+        init: init,
+        getWindow: getWindow
+    };
+};
