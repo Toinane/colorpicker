c0795e61326cde511af8ad22c34eb07139088cba
Cassidy Linhares <cassidy.linhares@ontariotechu.net>
2021-02-27 13:56:29 -0500

    added more test

--- test/color.class.test.js
+++ test/color.class.test.js
@@ -18,4 +18,25 @@ describe('test case for colour class init', ()=>{
     it('r,g,b should be set true', ()=>{
         expect(cp.setColor(0,0,0)).to.be.equal(true);
     });
+
+    it('r should be 0', ()=>{
+        expect(cp.red).to.be.equal(0);
+    }); 
+    
+    it('g should be 0', ()=>{
+        expect(cp.green).to.be.equal(0);
+    });  
+
+    it('b should be 0', ()=>{
+        expect(cp.blue).to.be.equal(0);
+    });  
+
+    it('r,g,b should be [0,0,0]', ()=>{
+        expect(cp.rgb).to.eql([0,0,0]);
+    }); 
+    
+    it('r,g,b,a should be [0,0,0,1]', ()=>{
+        expect(cp.rgba).to.eql([0,0,0,1]);
+    });
 });
+
