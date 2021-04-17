ee8df982b8c0dda054fab905778fc7d40a1032d6
Toinane <toinane@crea-that.fr>
2021-04-17 03:41:14 +0200

    feat(assets): update all assets

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -1,24 +1,25 @@
 require('dotenv/config')
 const builder = require('electron-builder')
 
-const isRelease = process.env.BUILD_RELEASE == true
+const isRelease = process.env.BUILD_RELEASE == 'true'
 const versionType = isRelease ? 'release' : 'nightly'
 const productName = isRelease ? 'Colorpicker' : 'Colorpicker Nightly'
 
 builder
     .build({
         config: {
-            appId: isRelease ? 'fr.toinane.colorpicjer' : 'fr.toinane.colorpicker-nightly',
+            appId: isRelease ? 'fr.toinane.colorpicker' : 'fr.toinane.colorpicker-nightly',
             productName: productName,
             copyright: 'Copyright © 2016 - 2021 Toinane',
             artifactName: '${productName}-${version}${arch}.${ext}',
             files: ['dist/**/*'],
-            compression: 'maximum',
+            compression: isRelease ? 'maximum' : 'store',
             buildDependenciesFromSource: true,
             directories: {
                 buildResources: 'assets',
                 output: 'release',
             },
+            publish: false,
             //afterSign: 'scripts/notarize.js',
             mac: {
                 target: ['dmg'],
@@ -56,17 +57,17 @@ builder
                 },
             },
             dmg: {
-                background: `assets/${versionType}/dmg-background.png`,
-                icon: `assets/${versionType}/dmg-installer.icns`,
+                background: `assets/${versionType}/dmg_background.png`,
+                icon: `assets/${versionType}/dmg_installer.icns`,
                 window: {
                     width: 540,
-                    height: 380,
+                    height: 310,
                 },
             },
             win: {
-                target: ['nsis', 'msi', 'portable'],
+                target: ['nsis', 'msi'],
                 icon: `assets/${versionType}/icon.ico`,
-                publisherName: 'Toinane',
+                publisherName: 'Crea-THAT',
                 verifyUpdateCodeSignature: false,
                 fileAssociations: [
                     {
@@ -81,11 +82,11 @@ builder
                 createDesktopShortcut: true,
                 createStartMenuShortcut: true,
                 allowToChangeInstallationDirectory: true,
-                installerIcon: `assets/${versionType}/nsis-installer-oldschool.ico`,
-                uninstallerIcon: `assets/${versionType}/nsis-uninstaller-oldschool.ico`,
-                installerHeader: `assets/${versionType}/installerHeader.bmp`,
-                installerSidebar: `assets/${versionType}/installerSidebar.bmp`,
-                uninstallerSidebar: `assets/${versionType}/uninstallerSidebar.bmp`,
+                installerIcon: `assets/${versionType}/nsis_installer_oldschool.ico`,
+                uninstallerIcon: `assets/${versionType}/nsis_installer_oldschool.ico`,
+                installerHeader: `assets/${versionType}/installer_header.bmp`,
+                installerSidebar: `assets/${versionType}/installer_sidebar.bmp`,
+                uninstallerSidebar: `assets/${versionType}/installer_sidebar.bmp`,
                 artifactName: '${productName}-${version}${arch}-installer.${ext}',
             },
             linux: {
